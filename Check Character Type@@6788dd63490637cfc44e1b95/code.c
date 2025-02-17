e24aa5b92898e3c51764f037147a200db2d2e78b
Himanshu <hthakur1628@gmail.com>
2025-02-17 20:54:23 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include<stido.h>
+int main (){
+    char ch;
+    printf("");
+    scanf("%c",ch);
+    if (ch == a,e,i,o,u){
+        printf("Vowels");
+    }
+    else (ch != a,e,i,o,u){
+        printf("Consonant")
+    }
+    return 0;
+}
