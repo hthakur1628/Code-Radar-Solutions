2ac0c5be83c88ee1efbb18a8dfec4adf5e30c634
Himanshu <hthakur1628@gmail.com>
2025-03-27 20:31:02 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -11,7 +11,7 @@ int main (){
     }
     void fibonacciSeries( int n){
         for (int i=0;i<=n;i++){
-            fibonacciSeries+=i;
+            fibonacciSeries += i;
         }
     }
     return 0;
