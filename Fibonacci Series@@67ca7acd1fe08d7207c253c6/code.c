be2e92aacd45dd82abb7885edfd3461a5452dc09
Himanshu <hthakur1628@gmail.com>
2025-03-27 20:30:36 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+void fibonacciSeries(int n);
+#include<stdio.h>
+int main (){
+    int n;
+    scanf("%d",&n);
+    if (n<=0){
+        printf("Invald input\n");
+        fibonacciSeries(n);
+        return 0;
+    }
+    void fibonacciSeries( int n){
+        for (i=0;i<=n;i++){
+            fibonacciSeries+=i
+        }
+    }
+    return 0;
+}
