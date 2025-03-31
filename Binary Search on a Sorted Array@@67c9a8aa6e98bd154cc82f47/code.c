9309abc8f3792db2149e454bb670dad144b33ea7
Himanshu <hthakur1628@gmail.com>
2025-03-31 22:39:43 +0530

    Solution to Binary Search on a Sorted Array

--- Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -1,30 +1,16 @@
 // Your code here...
 #include<stdio.h>
-int binarySearch(int arr[] , int size , int element );
+int binarySearch(int arr[] , int n , int target );
 int main (){
-    int arr [] = {1,2,4,45,65,68,69,70,89,90,100};
-    int size = sizeof(arr)/sizeof(int);
-    int element = 65;
-    int SearchIndex = binarySearch(arr , size , element );
-    printf("the element is %d was found at index %d \n ", element , SearchIndex);
-    return 0 ; 
+    int n , target ; 
+    scanf("%d",&n);
+    for (int i=0 ; i<n ; i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&target);
+    int result = binarySearch(arr,n,target);
+    printf("%d\n",result);
+    return 0 ;
 }
 
-int binarySearch(int arr[] , int size , int element ){
-    int low , mid , high ; 
-     low = 0;
-     high = size-1;
-    while(low <= high){
-        mid = (low + high)/2;
-        if (arr[mid]==element){
-            return mid;
-        }
-        if(arr[mid]<element){
-            low = mid + 1 ;
-        }
-        else {
-            high = mid - 1 ;
-        }
-    }
-    return -1;
-}
+int binarySearch(int arr[] , int n , int target )
