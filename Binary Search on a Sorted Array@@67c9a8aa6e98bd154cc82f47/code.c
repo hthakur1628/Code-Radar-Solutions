b6f14d1f06b0e17112fce8c9d246b9e69b68cdb7
Himanshu <hthakur1628@gmail.com>
2025-04-10 18:28:45 +0530

    Solution to Binary Search on a Sorted Array

--- Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -1,30 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int binarySearch(int arr[] , int n , int target){
-    int low = 0 , high = n - 1 ;
-    while (low <= high ){
-        int mid = (low + high )/2 ; 
-        if (arr[mid]==target ){
-            return mid ; 
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target) {
+            return mid;
         }
-        if (arr[mid]<target){
+        if (arr[mid] < target) {
             low = mid + 1;
-        }
-        else{
-            high = mid - 1 ;
+        } else {
+            high = mid - 1;
         }
     }
-    return -1 ;
+    return -1;
 }
-int main(){
-    int n , target ; 
-    scanf("%d",&n);
+
+int main() {
+    int n, target;
+    scanf("%d", &n); // Fixed format specifier
     int arr[n];
-    for (int i = 0 ; i < n ; i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    scanf("%d",&target);
-    int result = binarySearch(arr , n , target);
-    printf("%d\n",result);
-    return 0 ; 
+    scanf("%d", &target);
+    int result = binarySearch(arr, n, target);
+    printf("%d\n", result);
+    return 0;
 }
